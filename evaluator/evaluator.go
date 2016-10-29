a3d7639ced652855f62a055540f553758d4a3cbf
wata_mac <watassbass@gmail.com>
2016-10-29 22:25:03 +0900

    split file

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,11 +3,9 @@ package evaluator
 import (
 	"errors"
 	"fmt"
-	"reflect"
 	"regexp"
 	"strings"
 
-	"github.com/hashicorp/hcl"
 	hcl_ast "github.com/hashicorp/hcl/hcl/ast"
 	"github.com/hashicorp/hil"
 	hil_ast "github.com/hashicorp/hil/ast"
@@ -16,14 +14,6 @@ import (
 type Evaluator struct {
 	Config       hil.EvalConfig
 	ModuleConfig map[string]*hclModule
-}
-
-type hclVariable struct {
-	Name         string `hcl:",key"`
-	Default      interface{}
-	Description  string
-	DeclaredType string   `hcl:"type"`
-	Fields       []string `hcl:",decodedFields"`
 }
 
 func NewEvaluator(listmap map[string]*hcl_ast.ObjectList) (*Evaluator, error) {
@@ -46,84 +36,6 @@ func NewEvaluator(listmap map[string]*hcl_ast.ObjectList) (*Evaluator, error) {
 	}
 
 	return evaluator, nil
-}
-
-func detectVariables(listmap map[string]*hcl_ast.ObjectList) (map[string]hil_ast.Variable, error) {
-	varmap := make(map[string]hil_ast.Variable)
-
-	for _, list := range listmap {
-		var variables []*hclVariable
-		if err := hcl.DecodeObject(&variables, list.Filter("variable")); err != nil {
-			return nil, err
-		}
-
-		for _, v := range variables {
-			if v.Default == nil {
-				continue
-			}
-			varName := "var." + v.Name
-			varmap[varName] = parseVariable(v.Default, v.DeclaredType)
-		}
-	}
-
-	return varmap, nil
-}
-
-func parseVariable(val interface{}, varType string) hil_ast.Variable {
-	if varType == "" {
-		switch reflect.TypeOf(val).Kind() {
-		case reflect.String:
-			varType = "string"
-		case reflect.Slice:
-			varType = "list"
-		case reflect.Map:
-			varType = "map"
-		default:
-			varType = "string"
-		}
-	}
-
-	var hilVar hil_ast.Variable
-	switch varType {
-	case "string":
-		hilVar = hil_ast.Variable{
-			Type:  hil_ast.TypeString,
-			Value: val,
-		}
-	case "map":
-		fallthrough
-	case "list":
-		s := reflect.ValueOf(val)
-
-		switch reflect.TypeOf(s.Index(0).Interface()).Kind() {
-		case reflect.Map:
-			var variables map[string]hil_ast.Variable
-			variables = map[string]hil_ast.Variable{}
-			for i := 0; i < s.Len(); i++ {
-				ms := reflect.ValueOf(s.Index(i).Interface())
-				for _, k := range ms.MapKeys() {
-					key := fmt.Sprint(k.Interface())
-					value := fmt.Sprint(ms.MapIndex(reflect.ValueOf(key)).Interface())
-					variables[key] = parseVariable(value, "")
-				}
-			}
-			hilVar = hil_ast.Variable{
-				Type:  hil_ast.TypeMap,
-				Value: variables,
-			}
-		default:
-			var variables []hil_ast.Variable
-			for i := 0; i < s.Len(); i++ {
-				variables = append(variables, parseVariable(s.Index(i).Interface(), ""))
-			}
-			hilVar = hil_ast.Variable{
-				Type:  hil_ast.TypeList,
-				Value: variables,
-			}
-		}
-	}
-
-	return hilVar
 }
 
 func is_evaluable(src string) bool {
---
+++ evaluator/variable.go
@@ -0,0 +1,96 @@
+package evaluator
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/hashicorp/hcl"
+	hcl_ast "github.com/hashicorp/hcl/hcl/ast"
+	hil_ast "github.com/hashicorp/hil/ast"
+)
+
+type hclVariable struct {
+	Name         string `hcl:",key"`
+	Default      interface{}
+	Description  string
+	DeclaredType string   `hcl:"type"`
+	Fields       []string `hcl:",decodedFields"`
+}
+
+func detectVariables(listmap map[string]*hcl_ast.ObjectList) (map[string]hil_ast.Variable, error) {
+	varmap := make(map[string]hil_ast.Variable)
+
+	for _, list := range listmap {
+		var variables []*hclVariable
+		if err := hcl.DecodeObject(&variables, list.Filter("variable")); err != nil {
+			return nil, err
+		}
+
+		for _, v := range variables {
+			if v.Default == nil {
+				continue
+			}
+			varName := "var." + v.Name
+			varmap[varName] = parseVariable(v.Default, v.DeclaredType)
+		}
+	}
+
+	return varmap, nil
+}
+
+func parseVariable(val interface{}, varType string) hil_ast.Variable {
+	if varType == "" {
+		switch reflect.TypeOf(val).Kind() {
+		case reflect.String:
+			varType = "string"
+		case reflect.Slice:
+			varType = "list"
+		case reflect.Map:
+			varType = "map"
+		default:
+			varType = "string"
+		}
+	}
+
+	var hilVar hil_ast.Variable
+	switch varType {
+	case "string":
+		hilVar = hil_ast.Variable{
+			Type:  hil_ast.TypeString,
+			Value: val,
+		}
+	case "map":
+		fallthrough
+	case "list":
+		s := reflect.ValueOf(val)
+
+		switch reflect.TypeOf(s.Index(0).Interface()).Kind() {
+		case reflect.Map:
+			var variables map[string]hil_ast.Variable
+			variables = map[string]hil_ast.Variable{}
+			for i := 0; i < s.Len(); i++ {
+				ms := reflect.ValueOf(s.Index(i).Interface())
+				for _, k := range ms.MapKeys() {
+					key := fmt.Sprint(k.Interface())
+					value := fmt.Sprint(ms.MapIndex(reflect.ValueOf(key)).Interface())
+					variables[key] = parseVariable(value, "")
+				}
+			}
+			hilVar = hil_ast.Variable{
+				Type:  hil_ast.TypeMap,
+				Value: variables,
+			}
+		default:
+			var variables []hil_ast.Variable
+			for i := 0; i < s.Len(); i++ {
+				variables = append(variables, parseVariable(s.Index(i).Interface(), ""))
+			}
+			hilVar = hil_ast.Variable{
+				Type:  hil_ast.TypeList,
+				Value: variables,
+			}
+		}
+	}
+
+	return hilVar
+}
