ec8ae26f8afa68f19a89088daeab22f083420de9
wata_mac <watassbass@gmail.com>
2016-11-02 03:14:09 +0900

    add rich printer

--- issue/issue.go
+++ issue/issue.go
@@ -6,3 +6,20 @@ type Issue struct {
 	Line    int
 	File    string
 }
+
+type Issues []*Issue
+
+func (is Issues) Len() int      { return len(is) }
+func (is Issues) Swap(i, j int) { is[i], is[j] = is[j], is[i] }
+
+type ByFile struct {
+	Issues
+}
+
+func (b ByFile) Less(i, j int) bool { return b.Issues[i].File < b.Issues[j].File }
+
+type ByLine struct {
+	Issues
+}
+
+func (b ByLine) Less(i, j int) bool { return b.Issues[i].Line < b.Issues[j].Line }
--- printer/printer.go
+++ printer/printer.go
@@ -3,7 +3,10 @@ package printer
 import (
 	"fmt"
 	"io"
+	"sort"
+	"strconv"
 
+	"github.com/fatih/color"
 	"github.com/wata727/tflint/issue"
 )
 
@@ -11,6 +14,11 @@ type Printer struct {
 	stdout io.Writer
 	stderr io.Writer
 }
+
+var fileColor = color.New(color.Bold).SprintfFunc()
+var errorColor = color.New(color.FgRed).SprintFunc()
+var warningColor = color.New(color.FgYellow).SprintFunc()
+var noticeColor = color.New(color.FgHiWhite).SprintFunc()
 
 func Print(issues []*issue.Issue, stdout io.Writer, stderr io.Writer) {
 	printer := &Printer{
@@ -21,7 +29,67 @@ func Print(issues []*issue.Issue, stdout io.Writer, stderr io.Writer) {
 }
 
 func (p *Printer) Print(issues []*issue.Issue) {
-	for _, issue := range issues {
-		fmt.Fprintf(p.stdout, "%s: %s Line: %d in %s\n", issue.Type, issue.Message, issue.Line, issue.File)
+	sort.Sort(issue.ByFile{issue.Issues(issues)})
+	bIssue := issues[0]
+	sIssues := []*issue.Issue{
+		issues[0],
 	}
+
+	for _, is := range issues[1:] {
+		if bIssue.File != is.File {
+			p.printByLine(sIssues)
+			sIssues = nil
+		}
+
+		sIssues = append(sIssues, is)
+		bIssue = is
+	}
+	if sIssues != nil {
+		p.printByLine(sIssues)
+	}
+
+	p.printSummary(issues)
+}
+
+func (p *Printer) printByLine(issues []*issue.Issue) {
+	sort.Sort(issue.ByLine{issue.Issues(issues)})
+
+	fmt.Fprintf(p.stdout, "%s\n", fileColor(issues[0].File))
+	for _, i := range issues {
+		var issuePrefix string = i.Type + ":" + strconv.Itoa(i.Line)
+
+		switch i.Type {
+		case "ERROR":
+			issuePrefix = errorColor(issuePrefix)
+		case "WARNING":
+			issuePrefix = warningColor(issuePrefix)
+		case "NOTICE":
+			issuePrefix = noticeColor(issuePrefix)
+		}
+		fmt.Fprintf(p.stdout, "\t%s %s\n", issuePrefix, i.Message)
+	}
+}
+
+func (p *Printer) printSummary(issues []*issue.Issue) {
+	eIssues := []*issue.Issue{}
+	wIssues := []*issue.Issue{}
+	nIssues := []*issue.Issue{}
+
+	for _, i := range issues {
+		switch i.Type {
+		case "ERROR":
+			eIssues = append(eIssues, i)
+		case "WARNING":
+			wIssues = append(wIssues, i)
+		case "NOTICE":
+			nIssues = append(nIssues, i)
+		}
+	}
+
+	allResult := fileColor("All Issues: " + strconv.Itoa(len(issues)))
+	eResult := errorColor("error " + strconv.Itoa(len(eIssues)))
+	wResult := warningColor("warning " + strconv.Itoa(len(wIssues)))
+	nResult := noticeColor("notice " + strconv.Itoa(len(nIssues)))
+
+	fmt.Fprintf(p.stdout, "\n%s  %s , %s , %s\n", allResult, eResult, wResult, nResult)
 }
