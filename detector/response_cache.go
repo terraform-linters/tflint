b496c8d1bd65e7af25fd411a00a08bc7879d07f2
wata_mac <watassbass@gmail.com>
2016-12-24 18:24:30 +0900

    add invalid parameter group detector for ElastiCache

--- detector/response_cache.go
+++ detector/response_cache.go
@@ -2,18 +2,20 @@ package detector
 
 import (
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/elasticache"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/rds"
 )
 
 type ResponseCache struct {
-	DescribeImagesOutput            *ec2.DescribeImagesOutput
-	DescribeKeyPairsOutput          *ec2.DescribeKeyPairsOutput
-	DescribeSubnetsOutput           *ec2.DescribeSubnetsOutput
-	DescribeSecurityGroupsOutput    *ec2.DescribeSecurityGroupsOutput
-	ListInstanceProfilesOutput      *iam.ListInstanceProfilesOutput
-	DescribeInstancesOutput         *ec2.DescribeInstancesOutput
-	DescribeDBSubnetGroupsOutput    *rds.DescribeDBSubnetGroupsOutput
-	DescribeDBParameterGroupsOutput *rds.DescribeDBParameterGroupsOutput
-	DescribeOptionGroupsOutput      *rds.DescribeOptionGroupsOutput
+	DescribeImagesOutput               *ec2.DescribeImagesOutput
+	DescribeKeyPairsOutput             *ec2.DescribeKeyPairsOutput
+	DescribeSubnetsOutput              *ec2.DescribeSubnetsOutput
+	DescribeSecurityGroupsOutput       *ec2.DescribeSecurityGroupsOutput
+	ListInstanceProfilesOutput         *iam.ListInstanceProfilesOutput
+	DescribeInstancesOutput            *ec2.DescribeInstancesOutput
+	DescribeDBSubnetGroupsOutput       *rds.DescribeDBSubnetGroupsOutput
+	DescribeDBParameterGroupsOutput    *rds.DescribeDBParameterGroupsOutput
+	DescribeOptionGroupsOutput         *rds.DescribeOptionGroupsOutput
+	DescribeCacheParameterGroupsOutput *elasticache.DescribeCacheParameterGroupsOutput
 }
