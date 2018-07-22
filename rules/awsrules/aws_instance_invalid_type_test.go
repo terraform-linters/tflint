60a1d5de22c128f36006e0fe72971f864e54bf55
wata_mac <watassbass@gmail.com>
2018-07-22 17:26:24 +0900

    Revise tests

--- rules/awsrules/aws_instance_invalid_type_test.go
+++ rules/awsrules/aws_instance_invalid_type_test.go
@@ -1,6 +1,7 @@
 package awsrules
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -14,18 +15,29 @@ import (
 func Test_AwsInstanceInvalidType(t *testing.T) {
 	cases := []struct {
 		Name     string
-		Dir      string
+		Content  string
 		Expected issue.Issues
 	}{
 		{
 			Name: "basic",
-			Dir:  "basic",
+			Content: `
+resource "aws_instance" "invalid" {
+  instance_type = "t1.2xlarge"
+}
+
+resource "aws_instance" "valid" {
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "missing_key" {
+  ami = "ami-12345678"
+}`,
 			Expected: []*issue.Issue{
 				{
 					Detector: "aws_instance_invalid_type",
 					Type:     issue.ERROR,
 					Message:  "\"t1.2xlarge\" is invalid instance type.",
-					Line:     2,
+					Line:     3,
 					File:     "instances.tf",
 					Link:     "https://github.com/wata727/tflint/blob/master/docs/aws_instance_invalid_type.md",
 				},
@@ -33,17 +45,24 @@ func Test_AwsInstanceInvalidType(t *testing.T) {
 		},
 	}
 
-	dir, err := os.Getwd()
+	dir, err := ioutil.TempDir("", "AwsInstanceInvalidType")
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer os.RemoveAll(dir)
+
 	loader, err := configload.NewLoader(&configload.Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	for _, tc := range cases {
-		mod, diags := loader.Parser().LoadConfigDir(dir + "/test-fixtures/aws_instance_invalid_type/" + tc.Dir)
+		err := ioutil.WriteFile(dir+"/instances.tf", []byte(tc.Content), os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		mod, diags := loader.Parser().LoadConfigDir(dir)
 		if diags.HasErrors() {
 			panic(diags)
 		}
