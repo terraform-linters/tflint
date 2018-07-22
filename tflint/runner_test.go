158c27bbadbbb6dac06d53fda47b94c2f853315e
wata_mac <watassbass@gmail.com>
2018-07-22 17:26:24 +0900

    Revise tests

--- tflint/runner_test.go
+++ tflint/runner_test.go
@@ -3,6 +3,7 @@ package tflint
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
@@ -20,19 +21,27 @@ import (
 func Test_EvaluateExpr_string(t *testing.T) {
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		Expected  string
 	}{
 		{
-			Name:      "literal",
-			Key:       "literal",
+			Name: "literal",
+			Content: `
+resource "null_resource" "test" {
+  key = "literal_val"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  "literal_val",
 		},
 		{
 			Name: "string interpolation",
-			Key:  "string",
+			Content: `
+variable "string_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.string_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"string_var": cty.StringVal("string_val"),
@@ -42,7 +51,12 @@ func Test_EvaluateExpr_string(t *testing.T) {
 		},
 		{
 			Name: "new style interpolation",
-			Key:  "new_string",
+			Content: `
+variable "string_var" {}
+
+resource "null_resource" "test" {
+  key = var.string_var
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"string_var": cty.StringVal("string_val"),
@@ -52,7 +66,12 @@ func Test_EvaluateExpr_string(t *testing.T) {
 		},
 		{
 			Name: "list element",
-			Key:  "list_element",
+			Content: `
+variable "list_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.list_var[0]}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"list_var": cty.TupleVal([]cty.Value{cty.StringVal("one"), cty.StringVal("two")}),
@@ -62,7 +81,12 @@ func Test_EvaluateExpr_string(t *testing.T) {
 		},
 		{
 			Name: "map element",
-			Key:  "map_element",
+			Content: `
+variable "map_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.map_var["one"]}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"map_var": cty.ObjectVal(map[string]cty.Value{"one": cty.StringVal("one"), "two": cty.StringVal("two")}),
@@ -72,7 +96,12 @@ func Test_EvaluateExpr_string(t *testing.T) {
 		},
 		{
 			Name: "convert from integer",
-			Key:  "string",
+			Content: `
+variable "string_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.string_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"string_var": cty.NumberIntVal(10),
@@ -81,26 +110,40 @@ func Test_EvaluateExpr_string(t *testing.T) {
 			Expected: "10",
 		},
 		{
-			Name:      "conditional",
-			Key:       "conditional",
+			Name: "conditional",
+			Content: `
+resource "null_resource" "test" {
+  key = "${true ? "production" : "development"}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  "production",
 		},
 		{
-			Name:      "bulit-in function",
-			Key:       "function",
+			Name: "bulit-in function",
+			Content: `
+resource "null_resource" "test" {
+  key = "${md5("foo")}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  "acbd18db4cc2f85cedef654fccc4a4d8",
 		},
 		{
-			Name:      "terraform workspace",
-			Key:       "workspace",
+			Name: "terraform workspace",
+			Content: `
+resource "null_resource" "test" {
+  key = "${terraform.workspace}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  "default",
 		},
 		{
 			Name: "inside interpolation",
-			Key:  "inside",
+			Content: `
+variable "string_var" {}
+
+resource "null_resource" "test" {
+  key = "Hello ${var.string_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"string_var": cty.StringVal("World"),
@@ -110,12 +153,23 @@ func Test_EvaluateExpr_string(t *testing.T) {
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "string")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -137,13 +191,18 @@ func Test_EvaluateExpr_string(t *testing.T) {
 func Test_EvaluateExpr_integer(t *testing.T) {
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		Expected  int
 	}{
 		{
 			Name: "integer interpolation",
-			Key:  "integer",
+			Content: `
+variable "integer_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.integer_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"integer_var": cty.NumberIntVal(3),
@@ -153,7 +212,12 @@ func Test_EvaluateExpr_integer(t *testing.T) {
 		},
 		{
 			Name: "convert from string",
-			Key:  "integer",
+			Content: `
+variable "integer_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.integer_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{
 				"": map[string]cty.Value{
 					"integer_var": cty.StringVal("3"),
@@ -163,12 +227,23 @@ func Test_EvaluateExpr_integer(t *testing.T) {
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "integer")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -190,24 +265,38 @@ func Test_EvaluateExpr_integer(t *testing.T) {
 func Test_EvaluateExpr_stringList(t *testing.T) {
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		Expected  []string
 	}{
 		{
-			Name:      "list literal",
-			Key:       "string_list",
+			Name: "list literal",
+			Content: `
+resource "null_resource" "test" {
+  key = ["one", "two", "three"]
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  []string{"one", "two", "three"},
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "stringList")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -229,24 +318,38 @@ func Test_EvaluateExpr_stringList(t *testing.T) {
 func Test_EvaluateExpr_numberList(t *testing.T) {
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		Expected  []int
 	}{
 		{
-			Name:      "list literal",
-			Key:       "number_list",
+			Name: "list literal",
+			Content: `
+resource "null_resource" "test" {
+  key = [1, 2, 3]
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  []int{1, 2, 3},
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "numberList")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -273,24 +376,41 @@ func Test_EvaluateExpr_map(t *testing.T) {
 
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		Expected  mapObject
 	}{
 		{
-			Name:      "map literal",
-			Key:       "map",
+			Name: "map literal",
+			Content: `
+resource "null_resource" "test" {
+  key = {
+    one = 1
+    two = 2
+  }
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			Expected:  mapObject{One: 1, Two: 2},
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "map")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -312,54 +432,82 @@ func Test_EvaluateExpr_map(t *testing.T) {
 func Test_EvaluateExpr_interpolationError(t *testing.T) {
 	cases := []struct {
 		Name      string
-		Key       string
+		Content   string
 		Variables map[string]map[string]cty.Value
 		ErrorCode int
 		ErrorText string
 	}{
 		{
-			Name:      "undefined variable",
-			Key:       "undefined",
+			Name: "undefined variable",
+			Content: `
+resource "null_resource" "test" {
+  key = "${var.undefined_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			ErrorCode: EvaluationError,
-			ErrorText: "Failed to eval an expression in resource.tf:33; Reference to undeclared input variable: An input variable with the name \"undefined_var\" has not been declared. This variable can be declared with a variable \"undefined_var\" {} block.",
+			ErrorText: "Failed to eval an expression in resource.tf:3; Reference to undeclared input variable: An input variable with the name \"undefined_var\" has not been declared. This variable can be declared with a variable \"undefined_var\" {} block.",
 		},
 		{
-			Name:      "no default value",
-			Key:       "no_value",
+			Name: "no default value",
+			Content: `
+variable "no_value_var" {}
+
+resource "null_resource" "test" {
+  key = "${var.no_value_var}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			ErrorCode: UnknownValueError,
-			ErrorText: "Unknown value found in resource.tf:34; Please use environment variables or tfvars to set the value",
+			ErrorText: "Unknown value found in resource.tf:5; Please use environment variables or tfvars to set the value",
 		},
 		{
-			Name:      "terraform env",
-			Key:       "env",
+			Name: "terraform env",
+			Content: `
+resource "null_resource" "test" {
+  key = "${terraform.env}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			ErrorCode: EvaluationError,
-			ErrorText: "Failed to eval an expression in resource.tf:35; Invalid \"terraform\" attribute: The terraform.env attribute was deprecated in v0.10 and removed in v0.12. The \"state environment\" concept was rename to \"workspace\" in v0.12, and so the workspace name can now be accessed using the terraform.workspace attribute.",
+			ErrorText: "Failed to eval an expression in resource.tf:3; Invalid \"terraform\" attribute: The terraform.env attribute was deprecated in v0.10 and removed in v0.12. The \"state environment\" concept was rename to \"workspace\" in v0.12, and so the workspace name can now be accessed using the terraform.workspace attribute.",
 		},
 		{
-			Name:      "type mismatch",
-			Key:       "string_list",
+			Name: "type mismatch",
+			Content: `
+resource "null_resource" "test" {
+  key = ["one", "two", "three"]
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			ErrorCode: TypeConversionError,
-			ErrorText: "Invalid type expression in resource.tf:23; incorrect type; string required",
+			ErrorText: "Invalid type expression in resource.tf:3; incorrect type; string required",
 		},
 		{
-			Name:      "unevalauble",
-			Key:       "module",
+			Name: "unevalauble",
+			Content: `
+resource "null_resource" "test" {
+  key = "${module.text}"
+}`,
 			Variables: map[string]map[string]cty.Value{},
 			ErrorCode: UnevaluableError,
-			ErrorText: "Unevaluable expression found in resource.tf:38",
+			ErrorText: "Unevaluable expression found in resource.tf:3",
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "interpolationError")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -387,77 +535,128 @@ func Test_EvaluateExpr_interpolationError(t *testing.T) {
 func Test_isEvaluable(t *testing.T) {
 	cases := []struct {
 		Name     string
-		Key      string
+		Content  string
 		Expected bool
 	}{
 		{
-			Name:     "literal",
-			Key:      "literal",
+			Name: "literal",
+			Content: `
+resource "null_resource" "test" {
+  key = "literal_val"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "var syntax",
-			Key:      "string",
+			Name: "var syntax",
+			Content: `
+resource "null_resource" "test" {
+  key = "${var.string_var}"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "new var syntax",
-			Key:      "new_string",
+			Name: "new var syntax",
+			Content: `
+resource "null_resource" "test" {
+  key = var.string_var
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "conditional",
-			Key:      "conditional",
+			Name: "conditional",
+			Content: `
+resource "null_resource" "test" {
+  key = "${true ? "production" : "development"}"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "function",
-			Key:      "function",
+			Name: "function",
+			Content: `
+resource "null_resource" "test" {
+  key = "${md5("foo")}"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "terraform attributes",
-			Key:      "workspace",
+			Name: "terraform attributes",
+			Content: `
+resource "null_resource" "test" {
+  key = "${terraform.workspace}"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "include supported syntax",
-			Key:      "inside",
+			Name: "include supported syntax",
+			Content: `
+resource "null_resource" "test" {
+  key = "Hello ${var.string_var}"
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "list",
-			Key:      "string_list",
+			Name: "list",
+			Content: `
+
+resource "null_resource" "test" {
+  key = ["one", "two", "three"]
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "map",
-			Key:      "map",
+			Name: "map",
+			Content: `
+resource "null_resource" "test" {
+  key = {
+    one = 1
+    two = 2
+  }
+}`,
 			Expected: true,
 		},
 		{
-			Name:     "module",
-			Key:      "module",
+			Name: "module",
+			Content: `
+resource "null_resource" "test" {
+  key = "${module.text}"
+}`,
 			Expected: false,
 		},
 		{
-			Name:     "resource",
-			Key:      "resource",
+			Name: "resource",
+			Content: `
+resource "null_resource" "test" {
+  key = "${aws_subnet.app.id}"
+}`,
 			Expected: false,
 		},
 		{
-			Name:     "include unsupported syntax",
-			Key:      "unsupported",
+			Name: "include unsupported syntax",
+			Content: `
+resource "null_resource" "test" {
+  key = "${var.text} ${lookup(var.roles, count.index)}"
+}`,
 			Expected: false,
 		},
 	}
 
+	dir, err := ioutil.TempDir("", "isEvaluable")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for _, tc := range cases {
-		cfg, err := loadConfigHelper()
+		err := ioutil.WriteFile(dir+"/resource.tf", []byte(tc.Content), os.ModePerm)
 		if err != nil {
 			t.Fatal(err)
 		}
-		attribute, err := extractAttributeHelper(tc.Key, cfg)
+
+		cfg, err := loadConfigHelper(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		attribute, err := extractAttributeHelper("key", cfg)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -492,7 +691,7 @@ func Test_getWorkspace(t *testing.T) {
 		},
 		{
 			Name:     "environment file",
-			Dir:      filepath.Join(currentDir, "test-fixtures", "runner", "environment"),
+			Dir:      filepath.Join(currentDir, "test-fixtures", "with_environment_file"),
 			Expected: "staging",
 		},
 	}
@@ -550,18 +749,13 @@ func newRunnerHelper(cfg *configs.Config, variables map[string]map[string]cty.Va
 	}
 }
 
-func loadConfigHelper() (*configs.Config, error) {
+func loadConfigHelper(dir string) (*configs.Config, error) {
 	loader, err := configload.NewLoader(&configload.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	dir, err := os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-
-	mod, diags := loader.Parser().LoadConfigDir(dir + "/test-fixtures/runner")
+	mod, diags := loader.Parser().LoadConfigDir(dir)
 	if diags.HasErrors() {
 		return nil, diags
 	}
