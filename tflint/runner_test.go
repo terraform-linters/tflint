60c18581e544e4e5af891e4f22a3ec5d2ac6ccca
wata_mac <watassbass@gmail.com>
2018-08-07 01:43:21 +0900

    Use standard logger instead of custom logger

--- tflint/runner_test.go
+++ tflint/runner_test.go
@@ -1,16 +1,11 @@
 package tflint
 
 import (
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/hashicorp/hcl2/hcl"
-	"github.com/hashicorp/terraform/configs"
-	"github.com/hashicorp/terraform/configs/configload"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -748,44 +743,4 @@ resource "null_resource" "test" {
 			}
 		}
 	}
-}
-
-func loadConfigHelper(dir string) (*configs.Config, error) {
-	loader, err := configload.NewLoader(&configload.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	mod, diags := loader.Parser().LoadConfigDir(dir)
-	if diags.HasErrors() {
-		return nil, diags
-	}
-	cfg, diags := configs.BuildConfig(mod, configs.DisabledModuleWalker)
-	if diags.HasErrors() {
-		return nil, diags
-	}
-
-	return cfg, nil
-}
-
-func extractAttributeHelper(key string, cfg *configs.Config) (*hcl.Attribute, error) {
-	resource := cfg.Module.ManagedResources["null_resource.test"]
-	if resource == nil {
-		return nil, errors.New("Expected resource is not found")
-	}
-	body, _, diags := resource.Config.PartialContent(&hcl.BodySchema{
-		Attributes: []hcl.AttributeSchema{
-			{
-				Name: key,
-			},
-		},
-	})
-	if diags.HasErrors() {
-		return nil, diags
-	}
-	attribute := body.Attributes[key]
-	if attribute == nil {
-		return nil, fmt.Errorf("Expected attribute is not found: %s", key)
-	}
-	return attribute, nil
 }
