1dca51d1dd1af410737ad1698fe6f91116da1316
wata_mac <watassbass@gmail.com>
2018-08-07 01:43:21 +0900

    Use standard logger instead of custom logger

--- tflint/terraform.go
+++ tflint/terraform.go
@@ -3,6 +3,7 @@ package tflint
 import (
 	"bytes"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,7 +14,9 @@ import (
 
 func getTFDataDir() string {
 	dir := os.Getenv("TF_DATA_DIR")
-	if dir == "" {
+	if dir != "" {
+		log.Printf("[INFO] TF_DATA_DIR environment variable found: %s", dir)
+	} else {
 		dir = ".terraform"
 	}
 
@@ -30,12 +33,15 @@ func getTFModuleManifestPath() string {
 
 func getTFWorkspace() string {
 	if envVar := os.Getenv("TF_WORKSPACE"); envVar != "" {
+		log.Printf("[INFO] TF_WORKSPACE environment variable found: %s", envVar)
 		return envVar
 	}
 
 	envData, _ := ioutil.ReadFile(filepath.Join(getTFDataDir(), "environment"))
 	current := string(bytes.TrimSpace(envData))
-	if current == "" {
+	if current != "" {
+		log.Printf("[INFO] environment file found: %s", current)
+	} else {
 		current = "default"
 	}
 
@@ -50,6 +56,7 @@ func getTFEnvVariables() terraform.InputValues {
 		envVal := e[idx+1:]
 
 		if strings.HasPrefix(envKey, "TF_VAR_") {
+			log.Printf("[INFO] TF_VAR_* environment variable found: key=%s, value=%s", envKey, envVal)
 			varName := strings.Replace(envKey, "TF_VAR_", "", 1)
 
 			envVariables[varName] = &terraform.InputValue{
