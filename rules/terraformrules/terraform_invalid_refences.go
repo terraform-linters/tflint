29cb1f343e31de4520a2ab40c257fc2ceab7bd26
Anatoli Babenia <anatoli@rainforce.org>
2019-10-02 04:31:42 +0300

    Add boilerplate for invalid variable reference rule (#436)
    The following template reference is invalid, is treated as
    a literal string and not expanded.
    
        resource "null_resource" "generate_inventory" {
          triggers = {
            template_rendered = "$data.template_file.inventory_template.rendered"
          }
        ...

---
+++ rules/terraformrules/terraform_invalid_refences.go
@@ -0,0 +1,49 @@
+package terraformrules
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/wata727/tflint/tflint"
+)
+
+// TerraformInvalidReferencesRule checks for variable references without braces
+type TerraformInvalidReferencesRule struct{}
+
+// NewTerraformInvalidReferencesRule returns a new rule
+func NewTerraformInvalidReferencesRule() *TerraformInvalidReferencesRule {
+	return &TerraformInvalidReferencesRule{}
+}
+
+// Name returns the rule name
+func (r *TerraformInvalidReferencesRule) Name() string {
+	return "terraform_invalid_references"
+}
+
+// Enabled returns whether the rule is enabled by default
+func (r *TerraformInvalidReferencesRule) Enabled() bool {
+	return false
+}
+
+// Severity returns the rule severity
+func (r *TerraformInvalidReferencesRule) Severity() string {
+	return tflint.WARNING
+}
+
+// Link returns the rule reference link
+func (r *TerraformInvalidReferencesRule) Link() string {
+	return tflint.ReferenceLink(r.Name())
+}
+
+// Check goes over variables and checks is they are referenced without brances
+func (r *TerraformInvalidReferencesRule) Check(runner *tflint.Runner) error {
+	log.Printf("[TRACE] Check `%s` rule for `%s` runner", r.Name(), runner.TFConfigPath())
+
+	for _, variable := range runner.TFConfig.Module.Variables {
+		// iterate over resources
+		// find expandable strings
+		// match variable against strings content
+	}
+
+	return nil
+}
