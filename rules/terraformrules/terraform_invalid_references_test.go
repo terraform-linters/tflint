c7dbcedddbfbc8b907a121c6dbc8b7a2a26f7cfb
Anatoli Babenia <anatoli@rainforce.org>
2019-10-02 19:09:48 +0300

    Add test for missing braces rule.
    Signed-off-by: Anatoli Babenia <anatoli@rainforce.org>

---
+++ rules/terraformrules/terraform_invalid_references_test.go
@@ -0,0 +1,59 @@
+package terraformrules
+
+import (
+	"testing"
+
+	"github.com/hashicorp/hcl2/hcl"
+	"github.com/wata727/tflint/tflint"
+)
+
+func Test_TerraformInvalidReferencesRule(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Content  string
+		Expected tflint.Issues
+	}{
+		{
+			Name: "no braces",
+			Content: `
+resource "null_resource" "generate_inventory" {
+  triggers = {
+    template_rendered = "$data.template_file.inventory.rendered"
+  }
+}`,
+			Expected: tflint.Issues{
+				{
+					Rule:    NewTerraformInvalidReferencesRule(),
+					Message: "reference to ${data.template_file} is missing braces",
+					Range: hcl.Range{
+						Filename: "template_references.tf",
+						Start:    hcl.Pos{Line: 4, Column: 26},
+						End:      hcl.Pos{Line: 4, Column: 64},
+					},
+				},
+			},
+		},
+		{
+			Name: "with braces",
+			Content: `
+resource "null_resource" "generate_inventory" {
+  triggers = {
+    template_rendered = "${data.template_file.inventory.rendered}"
+  }
+}`,
+			Expected: tflint.Issues{},
+		},
+	}
+
+	rule := NewTerraformInvalidReferencesRule()
+
+	for _, tc := range cases {
+		runner := tflint.TestRunner(t, map[string]string{"template_references.tf": tc.Content})
+
+		if err := rule.Check(runner); err != nil {
+			t.Fatalf("Unexpected error occurred: %s", err)
+		}
+
+		tflint.AssertIssues(t, tc.Expected, runner.Issues)
+	}
+}
