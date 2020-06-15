4d88fae2e61236751e996593c10c4dfec4a6d7b5
Iain Adams <iwarapter@hotmail.com>
2020-06-15 17:26:49 +0100

    fix build issues

--- plugin/server.go
+++ plugin/server.go
@@ -56,6 +56,7 @@ func (s *Server) Blocks(req *tfplugin.BlocksRequest, resp *tfplugin.BlocksRespon
 	return nil
 }
 
+// Resources returns corresponding configs.Resource as tfplugin.Resource
 func (s *Server) Resources(req *tfplugin.ResourcesRequest, resp *tfplugin.ResourcesResponse) error {
 	var ret []*tfplugin.Resource
 	err := s.runner.WalkResources(req.Name, func(attr *configs.Resource) error {
