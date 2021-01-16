0b67f750b7cae05e73a7d07029d9f1ba410e7089
wata_mac <watassbass@gmail.com>
2021-01-16 17:52:57 +0900

    plugin: Fix segfault when accessing Config

--- plugin/encode.go
+++ plugin/encode.go
@@ -268,6 +268,9 @@ func (s *Server) encodeProvisioner(provisioner *tfconfigs.Provisioner) *tfplugin
 }
 
 func (s *Server) encodeBackend(backend *tfconfigs.Backend) *tfplugin.Backend {
+	if backend == nil {
+		return nil
+	}
 	configRange := tflint.HCLBodyRange(backend.Config, backend.DeclRange)
 	config := []byte{}
 	if configRange.Empty() {
