a11f482d92359af64d4e28ffe150bc564e0d26ad
Kazuma Watanabe <watassbass@gmail.com>
2022-11-06 01:27:03 +0900

    Convert variable types before applying defaults (#1582)

--- terraform/evaluator.go
+++ terraform/evaluator.go
@@ -248,14 +248,6 @@ func (d *evaluationData) GetInputVariable(addr addrs.InputVariable, rng hcl.Rang
 		val = config.Default
 	}
 
-	// Apply defaults from the variable's type constraint to the value,
-	// unless the value is null. We do not apply defaults to top-level
-	// null values, as doing so could prevent assigning null to a nullable
-	// variable.
-	if config.TypeDefaults != nil && !val.IsNull() {
-		val = config.TypeDefaults.Apply(val)
-	}
-
 	var err error
 	val, err = convert.Convert(val, config.ConstraintType)
 	if err != nil {
@@ -266,6 +258,14 @@ func (d *evaluationData) GetInputVariable(addr addrs.InputVariable, rng hcl.Rang
 			Subject:  &config.DeclRange,
 		})
 		val = cty.UnknownVal(config.Type)
+	}
+
+	// Apply defaults from the variable's type constraint to the value,
+	// unless the value is null. We do not apply defaults to top-level
+	// null values, as doing so could prevent assigning null to a nullable
+	// variable.
+	if config.TypeDefaults != nil && !val.IsNull() {
+		val = config.TypeDefaults.Apply(val)
 	}
 
 	// Mark if sensitive
