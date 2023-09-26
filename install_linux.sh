a5292a4d36f0f6332f7da3c630fc3fbc5690d877
ddelange <14880945+ddelange@users.noreply.github.com>
2023-09-26 18:25:12 +0300

    Typo

--- install_linux.sh
+++ install_linux.sh
@@ -50,7 +50,7 @@ download_path=$(mktemp -d -t tflint.XXXXXXXXXX)
 download_zip="${download_path}/tflint.zip"
 download_executable="${download_path}/tflint"
 
-version = "${TFLINT_VERSION:-latest}"
+version="${TFLINT_VERSION:-latest}"
 if [ "${version}" == "latest" ]; then
   echo "Looking up the latest version ..."
   if [ -n "${GITHUB_TOKEN}" ]; then
