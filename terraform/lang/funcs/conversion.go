59b86dc48a1772d8ef49d823e2669f21269668ab
Rui Chen <rui@chenrui.dev>
2026-04-19 22:56:44 -0400

    Port language helpers off BUSL
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- terraform/lang/funcs/conversion.go
+++ terraform/lang/funcs/conversion.go
@@ -1,5 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MPL-2.0
 
 package funcs
 
@@ -12,145 +11,102 @@ import (
 	"github.com/zclconf/go-cty/cty/function"
 )
 
-// MakeToFunc constructs a "to..." function, like "tostring", which converts
-// its argument to a specific type or type kind.
-//
-// The given type wantTy can be any type constraint that cty's "convert" package
-// would accept. In particular, this means that you can pass
-// cty.List(cty.DynamicPseudoType) to mean "list of any single type", which
-// will then cause cty to attempt to unify all of the element types when given
-// a tuple.
 func MakeToFunc(wantTy cty.Type) function.Function {
 	return function.New(&function.Spec{
-		Params: []function.Parameter{
-			{
-				Name: "v",
-				// We use DynamicPseudoType rather than wantTy here so that
-				// all values will pass through the function API verbatim and
-				// we can handle the conversion logic within the Type and
-				// Impl functions. This allows us to customize the error
-				// messages to be more appropriate for an explicit type
-				// conversion, whereas the cty function system produces
-				// messages aimed at _implicit_ type conversions.
-				Type:             cty.DynamicPseudoType,
-				AllowNull:        true,
-				AllowMarked:      true,
-				AllowDynamicType: true,
-				AllowUnknown:     true,
-			},
-		},
+		Params: []function.Parameter{dynamicConversionParam("v")},
 		Type: func(args []cty.Value) (cty.Type, error) {
-			gotTy := args[0].Type()
-			if gotTy.Equals(wantTy) {
-				return wantTy, nil
-			}
-			conv := convert.GetConversionUnsafe(args[0].Type(), wantTy)
-			if conv == nil {
-				// We'll use some specialized errors for some trickier cases,
-				// but most we can handle in a simple way.
-				switch {
-				case gotTy.IsTupleType() && wantTy.IsTupleType():
-					return cty.NilType, function.NewArgErrorf(0, "incompatible tuple type for conversion: %s", convert.MismatchMessage(gotTy, wantTy))
-				case gotTy.IsObjectType() && wantTy.IsObjectType():
-					return cty.NilType, function.NewArgErrorf(0, "incompatible object type for conversion: %s", convert.MismatchMessage(gotTy, wantTy))
-				default:
-					return cty.NilType, function.NewArgErrorf(0, "cannot convert %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
-				}
-			}
-			// If a conversion is available then everything is fine.
-			return wantTy, nil
+			return conversionResultType(args[0].Type(), wantTy)
 		},
 		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-			if !args[0].IsKnown() {
-				return cty.UnknownVal(retType).WithSameMarks(args[0]), nil
-			}
-
-			ret, err := convert.Convert(args[0], retType)
-			if err != nil {
-				val, _ := args[0].UnmarkDeep()
-				// Because we used GetConversionUnsafe above, conversion can
-				// still potentially fail in here. For example, if the user
-				// asks to convert the string "a" to bool then we'll
-				// optimistically permit it during type checking but fail here
-				// once we note that the value isn't either "true" or "false".
-				gotTy := val.Type()
-				switch {
-				case marks.Contains(args[0], marks.Sensitive):
-					// Generic message so we won't inadvertently disclose
-					// information about sensitive values.
-					return cty.NilVal, function.NewArgErrorf(0, "cannot convert this sensitive %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
-
-				case gotTy == cty.String && wantTy == cty.Bool:
-					what := "string"
-					if !val.IsNull() {
-						what = strconv.Quote(val.AsString())
-					}
-					return cty.NilVal, function.NewArgErrorf(0, `cannot convert %s to bool; only the strings "true" or "false" are allowed`, what)
-				case gotTy == cty.String && wantTy == cty.Number:
-					what := "string"
-					if !val.IsNull() {
-						what = strconv.Quote(val.AsString())
-					}
-					return cty.NilVal, function.NewArgErrorf(0, `cannot convert %s to number; given string must be a decimal representation of a number`, what)
-				default:
-					return cty.NilVal, function.NewArgErrorf(0, "cannot convert %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
-				}
-			}
-			return ret, nil
+			return convertValue(args[0], retType, wantTy)
 		},
 	})
 }
 
-// EphemeralAsNullFunc is a cty function that takes a value of any type and
-// returns a similar value with any ephemeral-marked values anywhere in the
-// structure replaced with a null value of the same type that is not marked
-// as ephemeral.
-//
-// This is intended as a convenience for returning the non-ephemeral parts of
-// a partially-ephemeral data structure through an output value that isn't
-// ephemeral itself.
 var EphemeralAsNullFunc = function.New(&function.Spec{
-	Params: []function.Parameter{
-		{
-			Name:             "value",
-			Type:             cty.DynamicPseudoType,
-			AllowDynamicType: true,
-			AllowUnknown:     true,
-			AllowNull:        true,
-			AllowMarked:      true,
-		},
-	},
+	Params: []function.Parameter{dynamicConversionParam("value")},
 	Type: func(args []cty.Value) (cty.Type, error) {
-		// This function always preserves the type of the given argument.
 		return args[0].Type(), nil
 	},
-	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-		return cty.Transform(args[0], func(p cty.Path, v cty.Value) (cty.Value, error) {
-			_, givenMarks := v.Unmark()
-			if _, isEphemeral := givenMarks[marks.Ephemeral]; isEphemeral {
-				// We'll strip the ephemeral mark but retain any other marks
-				// that might be present on the input.
-				delete(givenMarks, marks.Ephemeral)
-				if !v.IsKnown() {
-					// If the source value is unknown then we must leave it
-					// unknown because its final type might be more precise
-					// than the associated type constraint and returning a
-					// typed null could therefore over-promise on what the
-					// final result type will be.
-					// We're deliberately constructing a fresh unknown value
-					// here, rather than returning the one we were given,
-					// because we need to discard any refinements that the
-					// unknown value might be carrying that definitely won't
-					// be honored when we force the final result to be null.
-					return cty.UnknownVal(v.Type()).WithMarks(givenMarks), nil
-				}
-				return cty.NullVal(v.Type()).WithMarks(givenMarks), nil
-			}
-			return v, nil
-		})
+	Impl: func(args []cty.Value, _ cty.Type) (cty.Value, error) {
+		return cty.Transform(args[0], stripEphemeralValues)
 	},
 })
 
 func EphemeralAsNull(input cty.Value) (cty.Value, error) {
 	return EphemeralAsNullFunc.Call([]cty.Value{input})
+}
+
+func dynamicConversionParam(name string) function.Parameter {
+	return function.Parameter{
+		Name:             name,
+		Type:             cty.DynamicPseudoType,
+		AllowNull:        true,
+		AllowMarked:      true,
+		AllowDynamicType: true,
+		AllowUnknown:     true,
+	}
+}
+
+func conversionResultType(gotTy, wantTy cty.Type) (cty.Type, error) {
+	if gotTy.Equals(wantTy) {
+		return wantTy, nil
+	}
+	if convert.GetConversionUnsafe(gotTy, wantTy) != nil {
+		return wantTy, nil
+	}
+
+	switch {
+	case gotTy.IsTupleType() && wantTy.IsTupleType():
+		return cty.NilType, function.NewArgErrorf(0, "incompatible tuple type for conversion: %s", convert.MismatchMessage(gotTy, wantTy))
+	case gotTy.IsObjectType() && wantTy.IsObjectType():
+		return cty.NilType, function.NewArgErrorf(0, "incompatible object type for conversion: %s", convert.MismatchMessage(gotTy, wantTy))
+	default:
+		return cty.NilType, function.NewArgErrorf(0, "cannot convert %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
+	}
+}
+
+func convertValue(input cty.Value, retType cty.Type, wantTy cty.Type) (cty.Value, error) {
+	if !input.IsKnown() {
+		return cty.UnknownVal(retType).WithSameMarks(input), nil
+	}
+
+	converted, err := convert.Convert(input, retType)
+	if err == nil {
+		return converted, nil
+	}
+
+	unmarked, _ := input.UnmarkDeep()
+	gotTy := unmarked.Type()
+
+	switch {
+	case marks.Contains(input, marks.Sensitive):
+		return cty.NilVal, function.NewArgErrorf(0, "cannot convert this sensitive %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
+	case gotTy == cty.String && wantTy == cty.Bool:
+		return cty.NilVal, function.NewArgErrorf(0, `cannot convert %s to bool; only the strings "true" or "false" are allowed`, quotedStringValue(unmarked))
+	case gotTy == cty.String && wantTy == cty.Number:
+		return cty.NilVal, function.NewArgErrorf(0, `cannot convert %s to number; given string must be a decimal representation of a number`, quotedStringValue(unmarked))
+	default:
+		return cty.NilVal, function.NewArgErrorf(0, "cannot convert %s to %s", gotTy.FriendlyName(), wantTy.FriendlyNameForConstraint())
+	}
+}
+
+func quotedStringValue(value cty.Value) string {
+	if value.IsNull() {
+		return "string"
+	}
+	return strconv.Quote(value.AsString())
+}
+
+func stripEphemeralValues(_ cty.Path, value cty.Value) (cty.Value, error) {
+	_, valueMarks := value.Unmark()
+	if _, ok := valueMarks[marks.Ephemeral]; !ok {
+		return value, nil
+	}
+
+	delete(valueMarks, marks.Ephemeral)
+	if !value.IsKnown() {
+		return cty.UnknownVal(value.Type()).WithMarks(valueMarks), nil
+	}
+	return cty.NullVal(value.Type()).WithMarks(valueMarks), nil
 }
--- terraform/lang/funcs/conversion_test.go
+++ terraform/lang/funcs/conversion_test.go
@@ -1,5 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MPL-2.0
 
 package funcs
 
@@ -7,362 +6,138 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/terraform-linters/tflint-plugin-sdk/terraform/lang/marks"
-	"github.com/zclconf/go-cty-debug/ctydebug"
 	"github.com/zclconf/go-cty/cty"
 )
 
-func TestTo(t *testing.T) {
+func TestMakeToFunc(t *testing.T) {
 	tests := []struct {
-		Value    cty.Value
-		TargetTy cty.Type
-		Want     cty.Value
-		Err      string
+		name     string
+		value    cty.Value
+		targetTy cty.Type
+		want     cty.Value
+		wantErr  string
 	}{
 		{
-			cty.StringVal("a"),
-			cty.String,
-			cty.StringVal("a"),
-			``,
+			name:     "preserves matching type",
+			value:    cty.StringVal("hello"),
+			targetTy: cty.String,
+			want:     cty.StringVal("hello"),
 		},
 		{
-			cty.UnknownVal(cty.String),
-			cty.String,
-			cty.UnknownVal(cty.String),
-			``,
+			name:     "converts null dynamic to typed null",
+			value:    cty.NullVal(cty.DynamicPseudoType),
+			targetTy: cty.String,
+			want:     cty.NullVal(cty.String),
 		},
 		{
-			cty.NullVal(cty.String),
-			cty.String,
-			cty.NullVal(cty.String),
-			``,
+			name:     "preserves unknown marks",
+			value:    cty.UnknownVal(cty.String).Mark(marks.Ephemeral).Mark("trace"),
+			targetTy: cty.Bool,
+			want:     cty.UnknownVal(cty.Bool).Mark(marks.Ephemeral).Mark("trace"),
 		},
 		{
-			// This test case represents evaluating the expression tostring(null)
-			// from HCL, since null in HCL is cty.NullVal(cty.DynamicPseudoType).
-			// The result in that case should still be null, but a null specifically
-			// of type string.
-			cty.NullVal(cty.DynamicPseudoType),
-			cty.String,
-			cty.NullVal(cty.String),
-			``,
+			name:     "converts tuple to list",
+			value:    cty.TupleVal([]cty.Value{cty.StringVal("hello"), cty.True}),
+			targetTy: cty.List(cty.String),
+			want: cty.ListVal([]cty.Value{
+				cty.StringVal("hello"),
+				cty.StringVal("true"),
+			}),
 		},
 		{
-			cty.StringVal("a").Mark("boop"),
-			cty.String,
-			cty.StringVal("a").Mark("boop"),
-			``,
+			name:     "preserves nested marks",
+			value:    cty.ObjectVal(map[string]cty.Value{"value": cty.StringVal("world").Mark("nested")}).Mark("outer"),
+			targetTy: cty.Map(cty.String),
+			want: cty.MapVal(map[string]cty.Value{
+				"value": cty.StringVal("world").Mark("nested"),
+			}).Mark("outer"),
 		},
 		{
-			cty.NullVal(cty.String).Mark("boop"),
-			cty.String,
-			cty.NullVal(cty.String).Mark("boop"),
-			``,
+			name:     "string to bool error",
+			value:    cty.StringVal("nope"),
+			targetTy: cty.Bool,
+			wantErr:  `cannot convert "nope" to bool; only the strings "true" or "false" are allowed`,
 		},
 		{
-			cty.True,
-			cty.String,
-			cty.StringVal("true"),
-			``,
+			name:     "sensitive string to number error",
+			value:    cty.StringVal("secret").Mark(marks.Sensitive),
+			targetTy: cty.Number,
+			wantErr:  `cannot convert this sensitive string to number`,
 		},
 		{
-			cty.StringVal("a"),
-			cty.Bool,
-			cty.DynamicVal,
-			`cannot convert "a" to bool; only the strings "true" or "false" are allowed`,
-		},
-		{
-			cty.StringVal("a").Mark("boop"),
-			cty.Bool,
-			cty.DynamicVal,
-			`cannot convert "a" to bool; only the strings "true" or "false" are allowed`,
-		},
-		{
-			cty.StringVal("a").Mark(marks.Sensitive),
-			cty.Bool,
-			cty.DynamicVal,
-			`cannot convert this sensitive string to bool`,
-		},
-		{
-			cty.StringVal("a"),
-			cty.Number,
-			cty.DynamicVal,
-			`cannot convert "a" to number; given string must be a decimal representation of a number`,
-		},
-		{
-			cty.StringVal("a").Mark("boop"),
-			cty.Number,
-			cty.DynamicVal,
-			`cannot convert "a" to number; given string must be a decimal representation of a number`,
-		},
-		{
-			cty.StringVal("a").Mark(marks.Sensitive),
-			cty.Number,
-			cty.DynamicVal,
-			`cannot convert this sensitive string to number`,
-		},
-		{
-			cty.NullVal(cty.String),
-			cty.Number,
-			cty.NullVal(cty.Number),
-			``,
-		},
-		{
-			cty.UnknownVal(cty.Bool),
-			cty.String,
-			cty.UnknownVal(cty.String),
-			``,
-		},
-		{
-			cty.UnknownVal(cty.String),
-			cty.Bool,
-			cty.UnknownVal(cty.Bool), // conversion is optimistic
-			``,
-		},
-		{
-			cty.TupleVal([]cty.Value{cty.StringVal("hello"), cty.True}),
-			cty.List(cty.String),
-			cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("true")}),
-			``,
-		},
-		{
-			cty.TupleVal([]cty.Value{cty.StringVal("hello"), cty.True}),
-			cty.Set(cty.String),
-			cty.SetVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("true")}),
-			``,
-		},
-		{
-			cty.ObjectVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.True}),
-			cty.Map(cty.String),
-			cty.MapVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.StringVal("true")}),
-			``,
-		},
-		{
-			cty.ObjectVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.StringVal("world").Mark("boop")}),
-			cty.Map(cty.String),
-			cty.MapVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.StringVal("world").Mark("boop")}),
-			``,
-		},
-		{
-			cty.ObjectVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.StringVal("world")}).Mark("boop"),
-			cty.Map(cty.String),
-			cty.MapVal(map[string]cty.Value{"foo": cty.StringVal("hello"), "bar": cty.StringVal("world")}).Mark("boop"),
-			``,
-		},
-		{
-			cty.TupleVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world").Mark("boop")}),
-			cty.List(cty.String),
-			cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world").Mark("boop")}),
-			``,
-		},
-		{
-			cty.TupleVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world")}).Mark("boop"),
-			cty.List(cty.String),
-			cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world")}).Mark("boop"),
-			``,
-		},
-		{
-			cty.EmptyTupleVal,
-			cty.String,
-			cty.DynamicVal,
-			`cannot convert tuple to string`,
-		},
-		{
-			cty.UnknownVal(cty.EmptyTuple),
-			cty.String,
-			cty.DynamicVal,
-			`cannot convert tuple to string`,
-		},
-		{
-			cty.EmptyObjectVal,
-			cty.Object(map[string]cty.Type{"foo": cty.String}),
-			cty.DynamicVal,
-			`incompatible object type for conversion: attribute "foo" is required`,
-		},
-		{
-			cty.UnknownVal(cty.Object(map[string]cty.Type{"foo": cty.String})).Mark(marks.Ephemeral).Mark("boop"),
-			cty.Map(cty.String),
-			cty.UnknownVal(cty.Map(cty.String)).Mark(marks.Ephemeral).Mark("boop"),
-			``,
-		},
-		{
-			cty.ObjectVal(map[string]cty.Value{
-				"foo": cty.StringVal("hello"),
-				"bar": cty.StringVal("world").Mark("beep"),
-			}).Mark("boop"),
-			cty.Map(cty.String),
-			cty.MapVal(map[string]cty.Value{
-				"foo": cty.StringVal("hello"),
-				"bar": cty.StringVal("world").Mark("beep"),
-			}).Mark("boop"),
-			``,
+			name:     "object shape mismatch",
+			value:    cty.EmptyObjectVal,
+			targetTy: cty.Object(map[string]cty.Type{"foo": cty.String}),
+			wantErr:  `incompatible object type for conversion: attribute "foo" is required`,
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("to %s(%#v)", test.TargetTy.FriendlyNameForConstraint(), test.Value), func(t *testing.T) {
-			f := MakeToFunc(test.TargetTy)
-			got, err := f.Call([]cty.Value{test.Value})
-
-			if test.Err != "" {
-				if err == nil {
-					t.Fatal("succeeded; want error")
-				}
-				if got, want := err.Error(), test.Err; got != want {
-					t.Fatalf("wrong error\ngot:  %s\nwant: %s", got, want)
-				}
-				return
-			} else if err != nil {
-				t.Fatalf("unexpected error: %s", err)
-			}
-
-			if !got.RawEquals(test.Want) {
-				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
-			}
+		t.Run(test.name, func(t *testing.T) {
+			got, err := MakeToFunc(test.targetTy).Call([]cty.Value{test.value})
+			assertCtyResult(t, got, err, test.want, test.wantErr)
 		})
 	}
 }
 
 func TestEphemeralAsNull(t *testing.T) {
 	tests := []struct {
-		Input cty.Value
-		Want  cty.Value
+		name  string
+		input cty.Value
+		want  cty.Value
 	}{
-		// Simple cases
 		{
-			cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-			cty.NullVal(cty.String),
+			name:  "simple value becomes null",
+			input: cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
+			want:  cty.NullVal(cty.String),
 		},
 		{
-			cty.StringVal("hello"),
-			cty.StringVal("hello"),
+			name:  "unknown ephemeral stays unknown",
+			input: cty.UnknownVal(cty.String).RefineNotNull().Mark(marks.Ephemeral),
+			want:  cty.UnknownVal(cty.String),
 		},
 		{
-			// Unknown values stay unknown because an unknown value with
-			// an imprecise type constraint is allowed to take on a more
-			// precise type in later phases, but known values (even if null)
-			// should not. We do know that the final known result definitely
-			// won't be ephemeral, though.
-			cty.UnknownVal(cty.String).Mark(marks.Ephemeral),
-			cty.UnknownVal(cty.String),
-		},
-		{
-			cty.UnknownVal(cty.String),
-			cty.UnknownVal(cty.String),
-		},
-		{
-			// Unknown value refinements should be discarded when unmarking,
-			// both because we know our final value is going to be null
-			// anyway and because an ephemeral value is not required to
-			// have consistent refinements between the plan and apply phases.
-			cty.UnknownVal(cty.String).RefineNotNull().Mark(marks.Ephemeral),
-			cty.UnknownVal(cty.String),
-		},
-		{
-			// Refinements must be preserved for non-ephemeral values, though.
-			cty.UnknownVal(cty.String).RefineNotNull(),
-			cty.UnknownVal(cty.String).RefineNotNull(),
-		},
-
-		// Should preserve other marks in all cases
-		{
-			cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral).Mark(marks.Sensitive),
-			cty.NullVal(cty.String).Mark(marks.Sensitive),
-		},
-		{
-			cty.StringVal("hello").Mark(marks.Sensitive),
-			cty.StringVal("hello").Mark(marks.Sensitive),
-		},
-		{
-			cty.UnknownVal(cty.String).Mark(marks.Ephemeral).Mark(marks.Sensitive),
-			cty.UnknownVal(cty.String).Mark(marks.Sensitive),
-		},
-		{
-			cty.UnknownVal(cty.String).Mark(marks.Sensitive),
-			cty.UnknownVal(cty.String).Mark(marks.Sensitive),
-		},
-		{
-			cty.UnknownVal(cty.String).RefineNotNull().Mark(marks.Ephemeral).Mark(marks.Sensitive),
-			cty.UnknownVal(cty.String).Mark(marks.Sensitive),
-		},
-		{
-			cty.UnknownVal(cty.String).RefineNotNull().Mark(marks.Sensitive),
-			cty.UnknownVal(cty.String).RefineNotNull().Mark(marks.Sensitive),
-		},
-
-		// Nested ephemeral values
-		{
-			cty.ListVal([]cty.Value{
-				cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-				cty.StringVal("hello"),
+			name: "nested values are rewritten recursively",
+			input: cty.ObjectVal(map[string]cty.Value{
+				"stable": cty.StringVal("hello"),
+				"secret": cty.StringVal("world").Mark(marks.Ephemeral).Mark("trace"),
 			}),
-			cty.ListVal([]cty.Value{
-				cty.NullVal(cty.String),
-				cty.StringVal("hello"),
-			}),
-		},
-		{
-			cty.TupleVal([]cty.Value{
-				cty.True,
-				cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-				cty.StringVal("hello"),
-			}),
-			cty.TupleVal([]cty.Value{
-				cty.True,
-				cty.NullVal(cty.String),
-				cty.StringVal("hello"),
-			}),
-		},
-		{
-			// Sets can't actually preserve individual element marks, so
-			// this gets treated as the entire set being ephemeral.
-			// (That's true of the input value, despite how it's written here,
-			// not just the result value; cty.SetVal does the simplification
-			// itself during the construction of the value.)
-			cty.SetVal([]cty.Value{
-				cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-				cty.StringVal("hello"),
-			}),
-			cty.NullVal(cty.Set(cty.String)),
-		},
-		{
-			cty.MapVal(map[string]cty.Value{
-				"addr":  cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-				"greet": cty.StringVal("hello"),
-			}),
-			cty.MapVal(map[string]cty.Value{
-				"addr":  cty.NullVal(cty.String),
-				"greet": cty.StringVal("hello"),
-			}),
-		},
-		{
-			cty.ObjectVal(map[string]cty.Value{
-				"addr":  cty.StringVal("127.0.0.1:12654").Mark(marks.Ephemeral),
-				"greet": cty.StringVal("hello").Mark(marks.Sensitive),
-				"happy": cty.True,
-				"both":  cty.NumberIntVal(2).WithMarks(cty.NewValueMarks(marks.Sensitive, marks.Ephemeral)),
-			}),
-			cty.ObjectVal(map[string]cty.Value{
-				"addr":  cty.NullVal(cty.String),
-				"greet": cty.StringVal("hello").Mark(marks.Sensitive),
-				"happy": cty.True,
-				"both":  cty.NullVal(cty.Number).Mark(marks.Sensitive),
+			want: cty.ObjectVal(map[string]cty.Value{
+				"stable": cty.StringVal("hello"),
+				"secret": cty.NullVal(cty.String).Mark("trace"),
 			}),
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.Input.GoString(), func(t *testing.T) {
-			got, err := EphemeralAsNull(test.Input)
-			if err != nil {
-				// This function is supposed to be infallible
-				t.Fatal(err)
-			}
-
-			if diff := cmp.Diff(test.Want, got, ctydebug.CmpOptions); diff != "" {
-				t.Errorf("wrong result\n%s", diff)
-			}
+		t.Run(test.name, func(t *testing.T) {
+			got, err := EphemeralAsNull(test.input)
+			assertCtyResult(t, got, err, test.want, "")
 		})
 	}
+}
+
+func assertCtyResult(t *testing.T, got cty.Value, err error, want cty.Value, wantErr string) {
+	t.Helper()
+
+	if wantErr != "" {
+		if err == nil {
+			t.Fatal("succeeded; want error")
+		}
+		if err.Error() != wantErr {
+			t.Fatalf("wrong error\ngot:  %s\nwant: %s", err.Error(), wantErr)
+		}
+		return
+	}
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !got.RawEquals(want) {
+		t.Fatalf("wrong result\ngot:  %s\nwant: %s", formatCtyValue(got), formatCtyValue(want))
+	}
+}
+
+func formatCtyValue(value cty.Value) string {
+	return fmt.Sprintf("%#v", value)
 }
